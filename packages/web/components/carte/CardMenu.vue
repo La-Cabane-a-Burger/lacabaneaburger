ddfe543098353c7bb19a4adc15ed4bcf8c23990a
Elouan <louloulfx1@hotmail.fr>
2021-01-20 14:58:48 +0100

    feat(carte): cards

---
+++ packages/web/components/carte/CardMenu.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="card-container">
+    <div v-for="data in myJson">
+      <vs-card
+        class="card"
+        v-if="data.productType === 'burger' && burger === true"
+      >
+        <template #title>
+          <h3>{{ data.productName }}</h3>
+        </template>
+        <template #img>
+          <img src="https://vuesax.com/foto5.png" alt="" />
+        </template>
+        <template #text>
+          <p>{{ data.ingredient }}</p>
+          <p style="margin-top: 25px">
+            SEUL {{ data.price_alone }}€ | MENU {{ data.price_menu }}€
+          </p>
+        </template>
+        <template #interactions>
+          <vs-button warn icon>
+            <i class="bx bx-heart"></i>
+          </vs-button>
+        </template>
+      </vs-card>
+      <vs-avatar v-if="data.productType === 'boisson' && burger === false">
+        <template #text> Lily </template>
+      </vs-avatar>
+    </div>
+  </div>
+</template>
+
+<script>
+import json from "./carte.json";
+export default {
+  data() {
+    return {
+      burger: true,
+      myJson: json,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.card-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.card {
+  margin-right: 15px;
+  margin-left: 15px;
+  margin-bottom: 50px;
+  width: 40vh;
+}
+</style>
---
+++ packages/web/pages/carte.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <CardMenu />
+  </div>
+</template>
+<script>
+import CardMenu from "~/components/carte/CardMenu.vue";
+
+export default {
+  components: {
+    CardMenu,
+  },
+};
+</script>
