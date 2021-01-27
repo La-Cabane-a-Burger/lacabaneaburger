53b57188f302bd37298a92b317d636806346feea
clem-pgx <yeticlem79@hotmail.fr>
2021-01-27 11:17:34 +0100

    feat (contact): add contact form

---
+++ packages/web/components/contact/InputForm.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="card-input">
+    	<label :for="value" class="card-input__label">{{ label }}</label>
+    	<input type="text" :id="value" class="card-input__input"><!-- v-model="value" -->
+    </div>
+</template>
+<script>
+export default {
+    props: ['value', 'label']
+}
+</script>
+<style>
+.card-input {
+margin-bottom: 20px;
+}
+.card-input__label {
+font-size: 14px;
+margin-bottom: 5px;
+font-weight: 500;
+color: #7e7e7e;
+width: 100%;
+display: block;
+user-select: none;
+}
+.card-input__input {
+width: 100%;
+height: 50px;
+border-radius: 5px;
+box-shadow: none;
+border: 1px solid #ced6e0;
+transition: all 0.3s ease-in-out;
+font-size: 18px;
+padding: 5px 15px;
+background: none;
+color: #343a3f;
+font-family: "Source Sans Pro", sans-serif;
+}
+.card-input__input:hover, .card-input__input:focus {
+border-color: #343a3f;
+}
+.card-input__input:focus {
+box-shadow: 0px 10px 20px -13px rgba(32, 56, 117, 0.35);
+}
+.card-input__input.-select {
+-webkit-appearance: none;
+background-size: 12px;
+background-position: 90% center;
+background-repeat: no-repeat;
+padding-right: 30px;
+}
+.card-input__input {
+flex: 1;
+margin-right: 15px;
+}
+.card-input__input:last-child {
+margin-right: 0;
+}
+</style>
