41524e76ba9f330488d9ce63bc9b7f69bef50ae1
marceaudavid <marceau.david@ynov.com>
2021-03-10 16:35:24 +0100

    feat(home): homepage and navbar integration

---
+++ packages/web/components/CardCarrousel.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="flex items-center justify-between">
+    <ChevronLeft
+      @click="onClickLeft"
+      class="w-full h-full mr-5 cursor-pointer sm:w-1/4 sm:h-1/4 lg:w-20 lg:h-20"
+    />
+    <div
+      ref="carrousel"
+      class="flex items-center justify-between overflow-y-auto"
+    >
+      <div class="flex flex-row justify-between flex-1 py-5">
+        <Card
+          v-for="card in cards"
+          :key="card.title"
+          :image="card.image"
+          :title="card.title"
+          :text="card.text"
+          class="mr-5"
+        />
+      </div>
+    </div>
+    <ChevronRight
+      @click="onClickRight"
+      class="w-full h-full ml-5 cursor-pointer sm:w-1/4 sm:h-1/4 h-1/12 lg:w-20 lg:h-20"
+    />
+  </div>
+</template>
+
+<script>
+import Card from "./Card";
+import ChevronLeft from "~/assets/icons/chevron-left.svg?inline";
+import ChevronRight from "~/assets/icons/chevron-right.svg?inline";
+
+export default {
+  name: "CardCarrousel",
+  components: {
+    Card,
+    ChevronLeft,
+    ChevronRight,
+  },
+  props: {
+    cards: Array,
+  },
+  methods: {
+    onClickLeft() {
+      this.$refs.carrousel.scrollBy({
+        left: -300,
+        behavior: "smooth",
+      });
+    },
+    onClickRight() {
+      this.$refs.carrousel.scrollBy({
+        left: 300,
+        behavior: "smooth",
+      });
+    },
+  },
+};
+</script>
+
+<style>
+</style>
--- packages/web/nuxt.config.js
+++ packages/web/nuxt.config.js
@@ -1,26 +1,20 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'web',
+    title: "web",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-    'vuesax/dist/vuesax.css'
-  ],
+  css: ["vuesax/dist/vuesax.css"],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-    '@/plugins/vuesax'
-  ],
+  plugins: ["@/plugins/vuesax"],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
@@ -28,23 +22,24 @@ export default {
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/typescript
-    '@nuxt/typescript-build',
-    '@nuxtjs/tailwindcss'
+    "@nuxt/typescript-build",
+    "@nuxtjs/tailwindcss"
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    "@nuxtjs/axios",
     // https://go.nuxtjs.dev/pwa
-    '@nuxtjs/pwa',
-    '@nuxtjs/apollo'
+    "@nuxtjs/pwa",
+    "@nuxtjs/apollo",
+    "@nuxtjs/svg"
   ],
 
   apollo: {
     clientConfigs: {
       default: {
-        httpEndpoint: 'http://localhost:4000',
+        httpEndpoint: "http://localhost:4000"
       }
     }
   },
@@ -53,6 +48,5 @@ export default {
   axios: {},
 
   // Build Configuration (https://go.nuxtjs.dev/config-build)
-  build: {
-  }
-}
+  build: {}
+};
