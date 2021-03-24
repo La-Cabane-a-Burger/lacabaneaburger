8e9ab52259bf125afc3161c38d50a81c87d40ae3
Valentin <valentinguibert@hotmail.fr>
2021-03-24 08:32:15 +0100

    feat(history): added order history component

---
+++ packages/web/components/order-history-card.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="order-history-card shadow-lg px-2 py-3 text-left rounded">
+    <div class="flex items-center">
+      <img class="w-32 h-32 object-cover rounded-full" src="../assets/img/burger2.jpg" alt="">
+      <div class="ml-5">
+        <h3>{{ formattedDate }}</h3>
+        <p>{{ price }}€</p>
+        <p v-if="items">{{items.length}} plat</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    date: String,
+    price: Number,
+    items: Array
+  },
+  computed: {
+    formattedDate() {
+      const DDate = new Date(this.date);
+      const jour = DDate.getDate();
+      return jour
+    }
+  }
+}
+</script>
---
+++ packages/web/pages/order-history.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="order-history font-extrabold p-4 text-center">
+    <h2>Vos commandes</h2>
+    {{data.orders}}
+    <order-history-card :date="data.orders[0].date" :price="data.orders[0].price" :items="data.orders[0].items" />
+  </div>
+</template>
+
+<script>
+import orderHistoryCard from '~/components/order-history-card.vue'
+import data from '../orders.json';
+
+export default {
+  components:{
+
+    orderHistoryCard
+  },
+  data(){
+    return {
+      data: data
+    }
+  }
+}
+
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+.order-history{
+  font-family: 'Poppins', sans-serif;
+}
+</style>
