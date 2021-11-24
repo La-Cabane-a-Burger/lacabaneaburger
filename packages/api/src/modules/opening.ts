df99fed6a7d4ecf8f5b665669a9896bc1178106b
clem-pgx <yeticlem79@hotmail.fr>
2021-11-24 13:59:39 +0100

    feat(opening): crud

--- packages/api/src/modules/opening.ts
+++ packages/api/src/modules/opening.ts
@@ -1,11 +1,84 @@
 export const Opening = `
     type Opening {
         id: ID!
-        weekday: Int
+        weekday: String
         start: String
         end: String
         store: Store
     }
+
+    type Query {
+        getOpenings: [Opening]
+        getOpening(id: ID!): Opening
+    }
+
+    type Mutation {
+        createOpening(input: CreateOpeningInput!): Opening
+        deleteOpening(id: ID!): Opening
+        updateOpening(id: ID!, input: UpdateOpeningInput!): Opening
+    }
+
+    input CreateOpeningInput {
+        weekday: String
+        start: String
+        end: String
+        storeId: String
+    }
+
+    input UpdateOpeningInput {
+        weekday: String
+        start: String
+        end: String
+        storeId: String
+    }
 `
 
-export const OpeningResolver = {};
+interface CreateOpeningInput {
+    weekday: number,
+    start: string,
+    end: string,
+    storeId: string
+}
+
+interface UpdateOpeningInput {
+    weekday: number,
+    start: string,
+    end: string,
+    storeId: string
+}
+
+export const OpeningResolver = {
+    Query: {
+        getOpenings: (_parents: any, _args: any, ctx: Context) => {
+            return ctx.prisma.opening.findMany();
+        },
+        getOpening: (_parents: any, _args: { id: string }, ctx: Context) => {
+            return ctx.prisma.opening.findUnique({
+                where: {id: _args.id}
+            });
+        }
+    },
+    Mutation: {
+        createOpening: async (_parent: any, args: { input: CreateOpeningInput }, ctx: Context) => {
+
+            let opening = await ctx.prisma.opening.create({
+                data: {...args.input}
+            })
+
+            return opening;
+        },
+        updateOpening: async (_parent: any, args: { id: string, input: UpdateOpeningInput }, ctx: Context) => {
+            const opening = await ctx.prisma.opening.update({
+                where: {id: args.id},
+                data: {...args.input},
+            })
+            return opening;
+        },
+        deleteOpening: (_parents: any, args: { id: string }, ctx: Context) => {
+            return ctx.prisma.opening.delete({
+                where: {id: args.id}
+            })
+
+        }
+    }
+};
