bcf0e02761f96ee67fb973bf8d6b710178bfccb2
Axel-Ollivier <axel.ollivier56@outlook.com>
2021-12-08 08:36:35 +0100

    feat(api/menu): bonjour

--- packages/api/src/modules/menu.ts
+++ packages/api/src/modules/menu.ts
@@ -1,12 +1,96 @@
+import { Context } from '../context'
 export const Menu = `
     type Menu {
         id: ID!
-        name: Int
-        price: String
+        name: String
+        price: Float
         category: String
         description: String
-        items: [Item]
+        MenuItems: [String]
     }
+
+    type Query {
+        getMenus: [Menu]
+        getMenu(id: ID!): Menu
+    }
+    
+    type Mutation {
+        createMenu(input: CreateMenuInput): Menu
+        updateMenu(id: ID!, input: UpdateMenuInput): Menu
+        deleteMenu(id: ID!): Menu
+    }
+
+    input CreateMenuInput {
+        name: String
+        price: Float
+        category: String
+        description: String
+        MenuItems: [String]
+    }
+
+    input UpdateMenuInput {
+        name: String
+        price: Float
+        category: String
+        description: String
+        MenuItems: [String]
+    }
+    
 `
 
-export const MenuResolver = {};
+interface CreateMenuInput {
+  name: string
+  price: number
+  category: string
+  description: string
+  MenuItems: string[]
+}
+
+interface UpdateMenuInput {
+  name: string
+  price: number
+  category: string
+  description: string
+  MenuItems: string[]
+}
+
+export const MenuResolver = {
+  Query: {
+    getMenus: async (_parent: any, _args: any, ctx: Context) => {
+      return ctx.prisma.menu.findMany()
+    },
+    getMenu: async (_parent: any, args: any, ctx: Context) => {
+      return ctx.prisma.menu.findUnique({
+        where: { id: args.id },
+      })
+    },
+  },
+  Mutation: {
+    createMenu: async (
+      _parent: any,
+      args: { input: CreateMenuInput },
+      ctx: Context,
+    ) => {
+      let menu = await ctx.prisma.menu.create({
+        data: { ...args.input },
+      })
+      return menu
+    },
+    updateMenu: async (
+      _parent: any,
+      args: { id: string; input: UpdateMenuInput },
+      ctx: Context,
+    ) => {
+      const menu = await ctx.prisma.menu.update({
+        where: { id: args.id },
+        data: { ...args.input },
+      })
+      return menu
+    },
+    deleteMenu: async (_parent: any, args: { id: string }, ctx: Context) => {
+      return ctx.prisma.menu.delete({
+        where: { id: args.id },
+      })
+    },
+  },
+}
