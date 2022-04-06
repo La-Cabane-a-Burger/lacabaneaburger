a7a37b0cdf0e45cf5261d5d3afedf4227e346612
clem-pgx <yeticlem79@hotmail.fr>
2022-04-06 08:42:04 +0200

    feat(front): add page infos restaurant

--- packages/landing/pages/restaurant/[name].vue
+++ packages/landing/pages/restaurant/[name].vue
@@ -9,9 +9,9 @@
         backgroundSize: 'cover',
       }"
     >
-      <div class="flex flex-row w-10/12 mt-48 mb-12 bg-white shadow-lg h-4/6 rounded-xl">
+      <div class="flex flex-col lg:flex-row w-10/12 mt-48 mb-12 bg-white shadow-lg h-4/6 rounded-xl">
         <div class="flex-[0_1_50%] p-10">
-          <Subtitle class="ml-5" :top-text="'La cabane à burger - '" />
+          <Subtitle class="ml-5" :top-text="'La cabane à burger - ' + myStore.city" />
           <div class="flex">
             <div>
               <LocationMarkerIcon class="w-7 mr-5" />
@@ -31,41 +31,17 @@
               <p>Voir les horaires d'ouverture</p>
             </template>
             <template v-slot:content>
-              <table class="border-collapse border border-gray-400 ...">
+              <table class="border-collapse border border-gray-400 ... m-auto mt-5">
               <thead>
                 <tr>
-                  <th class="border border-white w-1/2 ..."></th>
-                  <th class="border border-white ...">Horaires</th>
+                  <th class="border border-white w-1/3 ..."></th>
+                  <th class="border border-white ..."></th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td class="border border-white w-1/2 ...">Lundi<!-- <div class="border-dashed border w-3/4 inline-block ml-1"></div> --></td>
-                  <td class="border border-white ...">11h30/13h45, 17h00/20h00</td>
-                </tr>
-                <tr>
-                  <td class="border border-white w-1/2 ...">Mardi</td>
-                  <td class="border border-white ...">11h30/13h45, 17h00/20h00</td>
-                </tr>
-                <tr>
-                  <td class="border border-white w-1/2 ...">Mercredi</td>
-                  <td class="border border-white ...">11h30/13h45, 17h00/20h00</td>
-                </tr>
-                <tr>
-                  <td class="border border-white w-1/2 ...">Jeudi</td>
-                  <td class="border border-white ...">11h30/13h45, 17h00/20h00</td>
-                </tr>
-                <tr>
-                  <td class="border border-white w-1/2 ...">Vendredi</td>
-                  <td class="border border-white ...">11h30/13h45, 17h00/20h00</td>
-                </tr>
-                <tr>
-                  <td class="border border-white w-1/2 ...">Samedi</td>
-                  <td class="border border-white ...">11h30/13h45, 17h00/20h00</td>
-                </tr>
-                <tr>
-                  <td class="border border-white w-1/2 ...">Dimanche</td>
-                  <td class="border border-white ...">11h30/13h45, 17h00/20h00</td>
+                <tr v-for="openning in formattedOpennings" v-bind:key="openning.day">
+                  <td class="border border-white w-1/2 ...">{{ openning.day }}</td>
+                  <td class="border border-white ...">{{ openning.time }}</td>
                 </tr>
               </tbody>
             </table>
@@ -78,26 +54,26 @@
             <Button class="mt-5 mx-5" secondary>COMMANDER</Button>
           </div>
           <hr class="w-full mt-5">
-          <div class="mt-5 inline-flex">
-            <div class="flex">
+          <div class="mt-5 flex justify-center flex-col sm:flex-row items-left">
+            <div class="flex mr-2">
               <div class="w-6 h-6 rounded-full inline-flex items-center justify-center bg-green-500 text-white mr-2"><CheckIcon class="w-5 inline"/>
               </div>
               <div>A emporter</div>
             </div>
 
-            <div class="flex ml-2">
+            <div class="flex mr-2">
               <div class="w-6 h-6 rounded-full inline-flex items-center justify-center bg-green-500 text-white mr-2"><CheckIcon class="w-5 inline"/>
               </div>
               <div>Sur place</div>
             </div>
-            <br>
-            <div class="flex ml-2">
+            
+            <div class="flex mr-2">
               <div class="w-6 h-6 rounded-full inline-flex items-center justify-center bg-green-500 text-white mr-2"><CheckIcon class="w-5 inline"/>
               </div>
               <div>En livraison</div>
             </div>
 
-            <div class="flex ml-2">
+            <div class="flex">
               <div class="w-6 h-6 rounded-full inline-flex items-center justify-center bg-green-500 text-white mr-2"><CheckIcon class="w-5 inline"/>
               </div>
               <div>En click & collect</div>
@@ -105,7 +81,7 @@
           </div>
         </div>
         <div class="flex-1">
-          <iframe :src="'https://www.google.com/maps?q='+ myStore.address +','+ myStore.postal_code + myStore.city +'&output=embed'" class="rounded-r-xl w-full h-full block" allowfullscreen="true" loading="lazy">
+          <iframe :src="'https://www.google.com/maps?q='+ myStore.address +','+ myStore.postal_code + myStore.city +'&output=embed'" class="lg:rounded-r-xl w-full h-full block h-80 lg:h-full" allowfullscreen="true" loading="lazy">
           </iframe>
         </div>
       </div>
@@ -113,8 +89,8 @@
   </div>
 </template>
 
-<script>
-import {defineComponent, defineAsyncComponent} from "vue";
+<script lang="ts">
+import {defineComponent, computed} from "vue";
 import { LocationMarkerIcon, CheckIcon } from "@heroicons/vue/solid";
 import Accordion from "../../components/Accordion.vue";
 import Button from "../../components/form/Button.vue";
@@ -125,8 +101,83 @@ export default defineComponent({
   setup() {
     const route = useRoute()
     const { result, loading } = useGetStoreBySlugQuery({slug: route.params.name })
-    const myStore = useResult(result, {})
-    return { myStore, loading }
+    const myStore = useResult(result, {openings: []})
+
+    const formattedOpennings = computed(() => {
+      if(myStore.value?.openings){
+        const weekdays = ["lundi", "mardi", "mercredi", "jeudi", "vendredi", "samedi", "dimanche"]
+        const finalWeek: { day: string; time: string; }[] = []
+        const finalDay: { day: string; time: string; }[] = []
+        let time1 = ""
+        const memory: string[] = []
+        let valid = false
+
+        function sortFunc(a: { day: string; }, b: { day: string; }) {
+          return weekdays.indexOf(a.day.toLowerCase()) - weekdays.indexOf(b.day.toLowerCase());
+        }
+
+        const weekOpenings = myStore.value.openings.map(function(opening) {
+          return opening?.start === opening?.end ? opening?.weekday + ':' +opening?.start : opening?.weekday + ':' +opening?.start + '/' + opening?.end
+        });
+        weekdays.forEach(weekday => {
+          weekOpenings.forEach(weekOpening => {
+            const formatWeek = weekOpening.split(':')
+            const objectWeek = {day:formatWeek[0], time:formatWeek[1]}
+            if(formatWeek[0].toLowerCase() === weekday){
+              finalWeek.push(objectWeek)
+            }
+          });
+        });
+        weekdays.forEach(weekday => {
+          finalWeek.forEach(day => {
+            if(weekday === day.day.toLowerCase()){
+              if(!memory.includes(weekday)){
+                time1 = day.time
+                valid = true
+              } else {
+                valid = false
+              }
+
+              if(time1 !== null && valid === true){
+                memory.push(weekday)
+                finalDay.push(day)
+              } else {
+                finalDay.splice(-1,1);
+                Object.keys(day).forEach((key) => {
+                  if (key === "time"){
+                    const arrTime1 = time1.length === 10 || time1.length === 9 ? "0" + time1 : time1
+                    const arrTime2 = day[key].length === 10 || day[key].length === 9 ? "0" + day[key] : day[key]
+                    const arr = [arrTime1, arrTime2]
+                    arr.sort();
+                    const hour = arr[0] === arrTime1 ? time1 + ', ' + day[key] : day[key] + ', ' + time1
+                    day[key] =  hour;
+                  }
+                });
+                finalDay.push(day)
+              }
+            }
+          });
+        });
+        if(finalDay.length !== 7) {
+          weekdays.forEach((weekday) => {
+            let end = false
+            finalDay.forEach((day, idx) =>{
+              if(weekday === day.day.toLowerCase()){
+                end = true
+              }
+              if(idx === finalDay.length-1 && !end){
+                finalDay.push({day:weekday.charAt(0).toUpperCase() + weekday.substring(1).toLowerCase(), time:"Fermé"})
+              }
+            });
+          });
+        }
+        finalDay.sort(sortFunc);
+        return finalDay
+      } else {
+        return {}
+      }
+    });
+    return { myStore, loading, formattedOpennings }
   }
-})
+});
 </script>
