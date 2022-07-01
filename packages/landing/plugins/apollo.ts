ecda6e5123b8cac3a90d1de7f5c5df30827d7c1f
Valentin <valentinguibert@hotmail.fr>
2022-07-01 14:51:15 +0200

    refactor(all): use env to link frontend to backend

--- packages/landing/plugins/apollo.ts
+++ packages/landing/plugins/apollo.ts
@@ -9,7 +9,7 @@ export default defineNuxtPlugin((nuxt: NuxtApp) => {
 
     const apolloClient = new ApolloClient({
         cache: new InMemoryCache(),
-        uri: 'http://localhost:4000',
+        uri: process.env.VUE_APP_API_URL || 'http://localhost:4000',
         ssrMode: true,
     })
     nuxt.vueApp.provide(DefaultApolloClient, apolloClient)
