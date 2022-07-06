0df87fb2f31da2e1bb58ab87b939099b564cd7a7
Valentin <valentinguibert@hotmail.fr>
2022-07-06 23:22:35 +0200

    feat(landing): add migration

--- packages/api/prisma/migrations/20220706212115_tables/migration.sql
+++ packages/api/prisma/migrations/20220706212115_tables/migration.sql
@@ -2,18 +2,18 @@
 CREATE TYPE "Role" AS ENUM ('USER', 'MANAGER', 'ADMIN');
 
 -- CreateEnum
-CREATE TYPE "Allergen" AS ENUM ('GLUTEN', 'PEANUTS', 'SOY', 'SHELLFISH', 'EGGS', 'MILK', 'NUTS', 'SESAME', 'WHEAT', 'CORN', 'SULFUR', 'LUPIN', 'MUSHROOMS', 'CELERY', 'MUSTARD', 'LAC', 'TURKEY', 'PORK', 'CHICKEN', 'FISH');
+CREATE TYPE "Allergen" AS ENUM ('GLUTEN', 'PEANUTS', 'SOY', 'SHELLFISH', 'EGGS', 'MILK', 'NUTS', 'SESAME', 'WHEAT', 'CORN', 'SULFUR', 'LUPIN', 'MUSHROOMS', 'CELERY', 'MUSTARD', 'MOLLUSCS', 'TURKEY', 'PORK', 'CHICKEN', 'FISH');
 
 -- CreateEnum
 CREATE TYPE "Category" AS ENUM ('BURGER', 'SALAD', 'NUGGETS', 'APPETIZER', 'DESSERT', 'SIDE', 'BEVERAGE');
 
 -- CreateTable
 CREATE TABLE "User" (
-    "id" TEXT NOT NULL,
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "role" "Role" NOT NULL DEFAULT E'USER',
+    "role" "Role" NOT NULL DEFAULT 'USER',
     "phone" TEXT NOT NULL,
     "reward_points" INTEGER NOT NULL DEFAULT 0,
 
@@ -22,61 +22,61 @@ CREATE TABLE "User" (
 
 -- CreateTable
 CREATE TABLE "Store" (
-    "id" TEXT NOT NULL,
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "city" TEXT NOT NULL,
     "slug" TEXT NOT NULL,
     "phone" TEXT NOT NULL,
     "postal_code" INTEGER NOT NULL,
     "address" TEXT NOT NULL,
-    "latitude" DECIMAL(65,30) NOT NULL,
-    "longitude" DECIMAL(65,30) NOT NULL,
+    "latitude" DOUBLE PRECISION NOT NULL,
+    "longitude" DOUBLE PRECISION NOT NULL,
 
     CONSTRAINT "Store_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Opening" (
-    "id" TEXT NOT NULL,
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "weekday" TEXT NOT NULL,
     "start" TEXT NOT NULL,
     "end" TEXT NOT NULL,
-    "storeId" TEXT,
+    "storeId" UUID,
 
     CONSTRAINT "Opening_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Order" (
-    "id" TEXT NOT NULL,
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "price" DOUBLE PRECISION NOT NULL,
     "date" TIMESTAMP(3) NOT NULL,
-    "storeId" TEXT,
-    "userId" TEXT,
+    "storeId" UUID,
+    "userId" UUID,
 
     CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "OrderItem" (
-    "id" TEXT NOT NULL,
-    "orderId" TEXT NOT NULL,
-    "itemId" TEXT NOT NULL,
-    "menuId" TEXT,
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "orderId" UUID NOT NULL,
+    "itemId" UUID NOT NULL,
+    "menuId" UUID,
 
     CONSTRAINT "OrderItem_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "OrderRecipe" (
-    "orderItemId" TEXT NOT NULL,
-    "ingredientId" TEXT NOT NULL,
+    "orderItemId" UUID NOT NULL,
+    "ingredientId" UUID NOT NULL,
 
     CONSTRAINT "OrderRecipe_pkey" PRIMARY KEY ("orderItemId","ingredientId")
 );
 
 -- CreateTable
 CREATE TABLE "Menu" (
-    "id" TEXT NOT NULL,
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "name" TEXT NOT NULL,
     "price" DOUBLE PRECISION NOT NULL,
     "category" TEXT NOT NULL,
@@ -87,8 +87,8 @@ CREATE TABLE "Menu" (
 
 -- CreateTable
 CREATE TABLE "MenuItem" (
-    "menuId" TEXT NOT NULL,
-    "itemId" TEXT NOT NULL,
+    "menuId" UUID NOT NULL,
+    "itemId" UUID NOT NULL,
     "pickable" BOOLEAN NOT NULL DEFAULT false,
 
     CONSTRAINT "MenuItem_pkey" PRIMARY KEY ("menuId","itemId")
@@ -96,20 +96,21 @@ CREATE TABLE "MenuItem" (
 
 -- CreateTable
 CREATE TABLE "Item" (
-    "id" TEXT NOT NULL,
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "name" TEXT NOT NULL,
     "price" DOUBLE PRECISION NOT NULL,
     "category" "Category" NOT NULL,
-    "description" TEXT NOT NULL,
-    "menuId" TEXT,
-    "itemId" TEXT,
+    "description" TEXT,
+    "menuId" UUID,
+    "itemId" UUID,
+    "storeId" UUID,
 
     CONSTRAINT "Item_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
 CREATE TABLE "Ingredient" (
-    "id" TEXT NOT NULL,
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
     "name" TEXT NOT NULL,
     "price" DOUBLE PRECISION NOT NULL DEFAULT 0.0,
     "category" TEXT NOT NULL,
@@ -120,8 +121,8 @@ CREATE TABLE "Ingredient" (
 
 -- CreateTable
 CREATE TABLE "Recipe" (
-    "itemId" TEXT NOT NULL,
-    "ingredientId" TEXT NOT NULL,
+    "itemId" UUID NOT NULL,
+    "ingredientId" UUID NOT NULL,
     "pickable" BOOLEAN NOT NULL DEFAULT false,
     "number" INTEGER NOT NULL DEFAULT 1,
     "quantity" DOUBLE PRECISION NOT NULL DEFAULT 0,
@@ -132,6 +133,9 @@ CREATE TABLE "Recipe" (
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Store_slug_key" ON "Store"("slug");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Menu_name_key" ON "Menu"("name");
@@ -146,19 +150,19 @@ CREATE UNIQUE INDEX "Ingredient_name_key" ON "Ingredient"("name");
 ALTER TABLE "Opening" ADD CONSTRAINT "Opening_storeId_fkey" FOREIGN KEY ("storeId") REFERENCES "Store"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Order" ADD CONSTRAINT "Order_storeId_fkey" FOREIGN KEY ("storeId") REFERENCES "Store"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "Order" ADD CONSTRAINT "Order_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Order" ADD CONSTRAINT "Order_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "Order" ADD CONSTRAINT "Order_storeId_fkey" FOREIGN KEY ("storeId") REFERENCES "Store"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_itemId_fkey" FOREIGN KEY ("itemId") REFERENCES "Item"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_menuId_fkey" FOREIGN KEY ("menuId") REFERENCES "Menu"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_menuId_fkey" FOREIGN KEY ("menuId") REFERENCES "Menu"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_itemId_fkey" FOREIGN KEY ("itemId") REFERENCES "Item"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "OrderRecipe" ADD CONSTRAINT "OrderRecipe_orderItemId_fkey" FOREIGN KEY ("orderItemId") REFERENCES "OrderItem"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
@@ -171,6 +175,9 @@ ALTER TABLE "MenuItem" ADD CONSTRAINT "MenuItem_menuId_fkey" FOREIGN KEY ("menuI
 
 -- AddForeignKey
 ALTER TABLE "MenuItem" ADD CONSTRAINT "MenuItem_itemId_fkey" FOREIGN KEY ("itemId") REFERENCES "Item"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Item" ADD CONSTRAINT "Item_storeId_fkey" FOREIGN KEY ("storeId") REFERENCES "Store"("id") ON DELETE SET NULL ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Item" ADD CONSTRAINT "Item_menuId_fkey" FOREIGN KEY ("menuId") REFERENCES "Menu"("id") ON DELETE SET NULL ON UPDATE CASCADE;
