685b63740cc9bdbe90a5eb537b5eb2c87205acab
Valentin <valentinguibert@hotmail.fr>
2022-07-13 08:56:17 +0200

    feat(landing): charge landing burgers on client side

--- packages/landing/pages/index.vue
+++ packages/landing/pages/index.vue
@@ -117,31 +117,39 @@
     >
       <div class="w-10/12 py-20">
         <Subtitle topText="Decouvrez" bottomText="nos burgers"/>
-        <CardCarrousel :loading="loading" :cards="burgersIntroduction"/>
-<!--        <div-->
-<!--            class="flex-row justify-between hidden p-10 my-20 lg:flex rounded-3xl bg-primary-400 font-headline"-->
-<!--        >-->
-<!--          <div class="flex-1">-->
-<!--            <h4 class="mb-2 text-lg font-extrabold uppercase text-dark">-->
-<!--              Le Burger du Mois-->
-<!--            </h4>-->
-<!--            <p class="text-xs text-dark lg:text-base">-->
-<!--              Chaque mois une nouvelle recette, imaginée par nos chefs pour vous-->
-<!--              proposer un burger de saison.-->
-<!--            </p>-->
-<!--          </div>-->
-<!--          <div class="flex items-center justify-center flex-1">-->
-<!--            <img-->
-<!--                src="@/assets/img/burger-board.png"-->
-<!--                alt="Burger sur une planche en bois"-->
-<!--            />-->
-<!--          </div>-->
-<!--          <div class="flex items-center justify-end flex-1">-->
-<!--            <Button secondary-->
-<!--            >Découvrir le burger du mois-->
-<!--            </Button>-->
-<!--          </div>-->
-<!--        </div>-->
+        <client-only>
+          <!-- this component will only be rendered on client-side -->
+          <CardCarrousel :loading="loading" :cards="burgersIntroduction"/>
+
+          <!-- loading indicator, rendered on server-side -->
+          <template #placeholder>
+            <CardCarrousel loading/>
+          </template>
+        </client-only>
+        <!--        <div-->
+        <!--            class="flex-row justify-between hidden p-10 my-20 lg:flex rounded-3xl bg-primary-400 font-headline"-->
+        <!--        >-->
+        <!--          <div class="flex-1">-->
+        <!--            <h4 class="mb-2 text-lg font-extrabold uppercase text-dark">-->
+        <!--              Le Burger du Mois-->
+        <!--            </h4>-->
+        <!--            <p class="text-xs text-dark lg:text-base">-->
+        <!--              Chaque mois une nouvelle recette, imaginée par nos chefs pour vous-->
+        <!--              proposer un burger de saison.-->
+        <!--            </p>-->
+        <!--          </div>-->
+        <!--          <div class="flex items-center justify-center flex-1">-->
+        <!--            <img-->
+        <!--                src="@/assets/img/burger-board.png"-->
+        <!--                alt="Burger sur une planche en bois"-->
+        <!--            />-->
+        <!--          </div>-->
+        <!--          <div class="flex items-center justify-end flex-1">-->
+        <!--            <Button secondary-->
+        <!--            >Découvrir le burger du mois-->
+        <!--            </Button>-->
+        <!--          </div>-->
+        <!--        </div>-->
         <div class="flex justify-center w-full">
           <Button @click="goToMenu">Voir la carte</Button>
         </div>
@@ -174,7 +182,7 @@ const goToFranchise = () => {
   router.push('/franchise')
 }
 
-const { result, loading } = useLandingItemsQuery();
+const {result, loading} = useLandingItemsQuery();
 
 const burgersIntroduction = computed(() => result?.value?.landingItems ?? []);
 
--- packages/landing/pages/mentions-legales.vue
+++ packages/landing/pages/mentions-legales.vue
@@ -1,54 +1,115 @@
 <template>
-    <div class="pt-48 md:px-24">
-        <div class="mt-10 grid gap-4 grid-cols-1">
-            <div class="m-auto vertical-middle">
-                <Subtitle bottom-text="Mentions légales"/>
-            </div>
-        </div>
-        <div class="p-10 pl-12">
-            <Subtitle top-text="1.Présentation du site :" bottom-text=""/>
-            <p class="mt-5 text-justify ...">Conformément aux dispositions des articles 6-III et 19 de la Loi n° 2004-575 du 21 juin 2004 pour la Confiance dans l'économie numérique, dite L.C.E.N., nous portons à la connaissance des utilisateurs et visiteurs du site lacabaneaburger.fr les informations suivantes<br><br>
-                Informations légales :<br>
-                Statut du propriétaire : SARL<br>
-                Nom de la Société : La Cabane à Burger<br>
-                Adresse : 2 bis Le Gué – 44160 CROSSAC<br>
-                SIRET : 751 893 405 000 17 R.C.S. : Nantes<br>
-                Adresse de courrier électronique : contact@lacabaneaburger.fr<br><br>
-                Les créateurs du site sont :<br><br>
-                Graphisme : <a class="hover:text-primary-600 underline" href="https://www.linkedin.com/in/valentin-guibert-63929b156/" target="_blank">Valentin Guibert</a>, <a class="hover:text-primary-600 underline" href="https://www.linkedin.com/in/axel-ollivier/" target="_blank">Axel Ollivier</a>, <a class="hover:text-primary-600 underline" href="https://www.linkedin.com/in/marceau-david-579b23177/" target="_blank">Marceau David</a> et <a class="hover:text-primary-600 underline" href="https://www.linkedin.com/in/cl%C3%A9ment-pageaux-758377172/" target="_blank">Clément Pageaux</a><br>
-                Développement et mise en ligne : <a class="hover:text-primary-600 underline" href="https://www.linkedin.com/in/valentin-guibert-63929b156/" target="_blank">Valentin Guibert</a>, <a class="hover:text-primary-600 underline" href="https://www.linkedin.com/in/axel-ollivier/" target="_blank">Axel Ollivier</a>, <a class="hover:text-primary-600 underline" href="https://www.linkedin.com/in/marceau-david-579b23177/" target="_blank">Marceau David</a> et <a class="hover:text-primary-600 underline" href="https://www.linkedin.com/in/cl%C3%A9ment-pageaux-758377172/" target="_blank">Clément Pageaux</a><br>
-                Création des contenus, rubriquage, conseil, gestion de projet : Com’Entre Nous<br>
-                Les Responsables de la publication sont : Laurent Thorel et Roland Chatel<br>
-                Les responsables de la publication est une personne physique.<br>
-                Le Webmaster est : <a class="hover:text-primary-600 underline" href="https://www.linkedin.com/in/valentin-guibert-63929b156/" target="_blank">Valentin Guibert</a><br>
-                L’hébergeur du site est : OVH<br>
-                Crédits : les mentions légales sont inspirées du site « generer-mentions- legales.com »
-            </p>
-        </div>
-        <div class="p-10 pl-12">
-            <Subtitle top-text="2.Description des services fournis :" bottom-text=""/>
-            <p class="mt-5 text-justify ...">Le site a pour objet de fournir une information concernant l’ensemble des activités de La Cabane à Burger dit La Cab. Le propriétaire du site s’efforce de fournir sur le site des informations aussi précises que possible. Toutefois, il ne pourra être tenue responsable des omissions, des inexactitudes et des carences dans la mise à jour, qu’elles soient de son fait ou du fait des tiers partenaires qui lui fournissent ces informations. Toutes les informations proposées sur le site sont données à titre indicatif, sont non exhaustives, et sont susceptibles d’évoluer. Elles sont données sous réserve de modifications ayant été apportées depuis leur mise en ligne.
-            </p>
-        </div>
-        <div class="p-10 pl-12">
-            <Subtitle top-text="3.Propriété intellectuelle et contrefaçons :" bottom-text=""/>
-            <p class="mt-5 text-justify ...">Le propriétaire du site est propriétaire des droits de propriété intellectuelle ou détient les droits d’usage sur tous les éléments accessibles sur le site, notamment les textes, images, graphismes, logos, icônes, sons, logiciels… Toute reproduction, représentation, modification, publication, adaptation totale ou partielle des éléments du site, quel que soit le moyen ou le procédé utilisé, est interdite, sauf autorisation écrite préalable délivrée par La Cabane à Burger.
-                <br><br>Toute exploitation non autorisée du site ou de l’un quelconque de ses éléments sera considérée comme constitutive d’une contrefaçon et poursuivie conformément aux dispositions des articles L.335-2 et suivants du Code de Propriété Intellectuelle.
-            </p>
-        </div>
-        <div class="p-10 pl-12">
-            <Subtitle top-text="4.Liens hypertextes et cookies :" bottom-text=""/>
-            <p class="mt-5 text-justify ...">Le site contient un certain nombre de liens hypertextes vers d’autres sites (partenaires, informations…) mis en place avec l’autorisation du propriétaire du site. Cependant, le propriétaire du site n’a pas la possibilité de vérifier le contenu des sites ainsi visités et décline donc toute responsabilité quant aux risques éventuels de contenus illicites.
-                <br><br>L’utilisateur est informé que lors de ses visites sur le site un ou des cookies sont susceptible(s) de s’installer automatiquement sur son ordinateur. Un cookie est un fichier de petite taille, qui ne permet pas l’identification de l’utilisateur, mais qui enregistre des informations relatives à la navigation d’un ordinateur sur un site. Les données ainsi obtenues visent à faciliter la navigation ultérieure sur le site, et ont également vocation à permettre diverses mesures de fréquentation.
-                <br><br>Le paramétrage du logiciel de navigation permet d’informer de la présence de cookie et éventuellement, de refuser de la manière décrite à l’adresse suivante : www.cnil.fr Le refus d’installation d’un cookie peut entraîner l’impossibilité d’accéder à certains services.
-            </p>
-        </div>
-        <div class="p-10 pl-12">
-            <Subtitle top-text="5.Protection des biens et des personnes - gestion des données personnelles :" bottom-text=""/>
-            <p class="mt-5 text-justify ...">Utilisateur : Internaute se connectant, utilisant le site susnommé : lacabaneaburger.fr En France, les données personnelles sont notamment protégées par la loi n° 78-87 du 6 janvier 1978, la loi n° 2004-801 du 6 août 2004, l'article L. 226-13 du Code pénal et la Directive Européenne du 24 octobre 1995. Sur le site, le propriétaire du site ne collecte des informations personnelles relatives à l'utilisateur que pour le besoin de certains services proposés par le site L'utilisateur fournit ces informations en toute connaissance de cause, notamment lorsqu'il procède par lui-même à leur saisie. Il est alors précisé à l'utilisateur du site l’obligation ou non de fournir ces informations.
-                <br><br>Conformément aux dispositions des articles 38 et suivants de la loi 78-17 du 6 janvier 1978 relative à l’informatique, aux fichiers et aux libertés, tout utilisateur dispose d’un droit d’accès, de rectification, de suppression et d’opposition aux données personnelles le concernant. Pour l’exercer, adressez votre demande à par email : contact@lacabaneaburger.fr ou en effectuant sa demande écrite et signée, accompagnée d’une copie du titre d’identité avec signature du titulaire de la pièce, en précisant l’adresse à laquelle la réponse doit être envoyée.
-                <br><br>Aucune information personnelle de l'utilisateur du site n'est publiée à l'insu de l'utilisateur, échangée, transférée, cédée ou vendue sur un support quelconque à des tiers. Seule l'hypothèse du rachat du site au propriétaire du site et de ses droits permettrait la transmission des dites informations à l'éventuel acquéreur qui serait à son tour tenu de la même obligation de conservation et de modification des données vis à vis de l'utilisateur du site www.lacabaneaburger.fr. Les bases de données sont protégées par les dispositions de la loi du 1er juillet 1998 transposant la directive 96/9 du 11 mars 1996 relative à la protection juridique des bases de données.
-            </p>
-        </div>
+  <div class="pt-48 md:px-24">
+    <div class="mt-10 grid gap-4 grid-cols-1">
+      <div class="m-auto vertical-middle">
+        <Subtitle bottom-text="Mentions légales"/>
+      </div>
     </div>
+    <div class="p-10 pl-12">
+      <Subtitle top-text="1.Présentation du site :" bottom-text=""/>
+      <p class="mt-5 text-justify ...">Conformément aux dispositions des articles 6-III et 19 de la Loi n° 2004-575 du
+        21 juin 2004 pour la Confiance dans l'économie numérique, dite L.C.E.N., nous portons à la connaissance des
+        utilisateurs et visiteurs du site lacabaneaburger.fr les informations suivantes<br><br>
+        Informations légales :<br>
+        Statut du propriétaire : SAS<br>
+        Nom de la Société : CAB Evolution (La Cabane à Burger)<br>
+        Adresse : 6 CHE DU PRE DES FLOTS 56130 FEREL<br>
+        SIRET : 850 905 423 000 16 R.C.S. : Vannes<br>
+        Adresse de courrier électronique : contact@lacabaneaburger.fr<br><br>
+        Les créateurs du site sont :<br><br>
+        Graphisme : <a class="hover:text-primary-600 underline"
+                       href="https://www.linkedin.com/in/valentin-guibert-63929b156/" target="_blank">Valentin
+          Guibert</a>, <a class="hover:text-primary-600 underline" href="https://www.linkedin.com/in/axel-ollivier/"
+                          target="_blank">Axel Ollivier</a>, <a class="hover:text-primary-600 underline"
+                                                                href="https://www.linkedin.com/in/marceau-david-579b23177/"
+                                                                target="_blank">Marceau David</a> et <a
+            class="hover:text-primary-600 underline" href="https://www.linkedin.com/in/cl%C3%A9ment-pageaux-758377172/"
+            target="_blank">Clément Pageaux</a><br>
+        Développement et mise en ligne : <a class="hover:text-primary-600 underline"
+                                            href="https://www.linkedin.com/in/valentin-guibert-63929b156/"
+                                            target="_blank">Valentin Guibert</a>, <a
+            class="hover:text-primary-600 underline" href="https://www.linkedin.com/in/axel-ollivier/" target="_blank">Axel
+          Ollivier</a>, <a class="hover:text-primary-600 underline"
+                           href="https://www.linkedin.com/in/marceau-david-579b23177/" target="_blank">Marceau David</a>
+        et <a class="hover:text-primary-600 underline"
+              href="https://www.linkedin.com/in/cl%C3%A9ment-pageaux-758377172/" target="_blank">Clément Pageaux</a><br>
+        Création des contenus, rubriquage, conseil, gestion de projet : Com’Entre Nous<br>
+        Les Responsables de la publication sont : Laurent Thorel et Roland Chatel<br>
+        Les responsables de la publication est une personne physique.<br>
+        Le Webmaster est : <a class="hover:text-primary-600 underline"
+                              href="https://www.linkedin.com/in/valentin-guibert-63929b156/" target="_blank">Valentin
+          Guibert</a><br>
+        L’hébergeur du site est : Netlify<br>
+      </p>
+    </div>
+    <div class="p-10 pl-12">
+      <Subtitle top-text="2.Description des services fournis :" bottom-text=""/>
+      <p class="mt-5 text-justify ...">Le site a pour objet de fournir une information concernant l’ensemble des
+        activités de La Cabane à Burger dit La Cab. Le propriétaire du site s’efforce de fournir sur le site des
+        informations aussi précises que possible. Toutefois, il ne pourra être tenue responsable des omissions, des
+        inexactitudes et des carences dans la mise à jour, qu’elles soient de son fait ou du fait des tiers partenaires
+        qui lui fournissent ces informations. Toutes les informations proposées sur le site sont données à titre
+        indicatif, sont non exhaustives, et sont susceptibles d’évoluer. Elles sont données sous réserve de
+        modifications ayant été apportées depuis leur mise en ligne.
+      </p>
+    </div>
+    <div class="p-10 pl-12">
+      <Subtitle top-text="3.Propriété intellectuelle et contrefaçons :" bottom-text=""/>
+      <p class="mt-5 text-justify ...">Le propriétaire du site est propriétaire des droits de propriété intellectuelle
+        ou détient les droits d’usage sur tous les éléments accessibles sur le site, notamment les textes, images,
+        graphismes, logos, icônes, sons, logiciels… Toute reproduction, représentation, modification, publication,
+        adaptation totale ou partielle des éléments du site, quel que soit le moyen ou le procédé utilisé, est
+        interdite, sauf autorisation écrite préalable délivrée par La Cabane à Burger.
+        <br><br>Toute exploitation non autorisée du site ou de l’un quelconque de ses éléments sera considérée comme
+        constitutive d’une contrefaçon et poursuivie conformément aux dispositions des articles L.335-2 et suivants du
+        Code de Propriété Intellectuelle.
+      </p>
+    </div>
+    <div class="p-10 pl-12">
+      <Subtitle top-text="4.Liens hypertextes et cookies :" bottom-text=""/>
+      <p class="mt-5 text-justify ...">Le site contient un certain nombre de liens hypertextes vers d’autres sites
+        (partenaires, informations…) mis en place avec l’autorisation du propriétaire du site. Cependant, le
+        propriétaire du site n’a pas la possibilité de vérifier le contenu des sites ainsi visités et décline donc toute
+        responsabilité quant aux risques éventuels de contenus illicites.
+        <br><br>L’utilisateur est informé que lors de ses visites sur le site un ou des cookies sont susceptible(s) de
+        s’installer automatiquement sur son ordinateur. Un cookie est un fichier de petite taille, qui ne permet pas
+        l’identification de l’utilisateur, mais qui enregistre des informations relatives à la navigation d’un
+        ordinateur sur un site. Les données ainsi obtenues visent à faciliter la navigation ultérieure sur le site, et
+        ont également vocation à permettre diverses mesures de fréquentation.
+        <br><br>Le paramétrage du logiciel de navigation permet d’informer de la présence de cookie et éventuellement,
+        de refuser de la manière décrite à l’adresse suivante : www.cnil.fr Le refus d’installation d’un cookie peut
+        entraîner l’impossibilité d’accéder à certains services.
+      </p>
+    </div>
+    <div class="p-10 pl-12">
+      <Subtitle top-text="5.Protection des biens et des personnes - gestion des données personnelles :" bottom-text=""/>
+      <p class="mt-5 text-justify ...">Utilisateur : Internaute se connectant, utilisant le site susnommé :
+        lacabaneaburger.fr En France, les données personnelles sont notamment protégées par la loi n° 78-87 du 6 janvier
+        1978, la loi n° 2004-801 du 6 août 2004, l'article L. 226-13 du Code pénal et la Directive Européenne du 24
+        octobre 1995. Sur le site, le propriétaire du site ne collecte des informations personnelles relatives à
+        l'utilisateur que pour le besoin de certains services proposés par le site L'utilisateur fournit ces
+        informations en toute connaissance de cause, notamment lorsqu'il procède par lui-même à leur saisie. Il est
+        alors précisé à l'utilisateur du site l’obligation ou non de fournir ces informations.
+        <br><br>Conformément aux dispositions des articles 38 et suivants de la loi 78-17 du 6 janvier 1978 relative à
+        l’informatique, aux fichiers et aux libertés, tout utilisateur dispose d’un droit d’accès, de rectification, de
+        suppression et d’opposition aux données personnelles le concernant. Pour l’exercer, adressez votre demande à par
+        email : contact@lacabaneaburger.fr ou en effectuant sa demande écrite et signée, accompagnée d’une copie du
+        titre d’identité avec signature du titulaire de la pièce, en précisant l’adresse à laquelle la réponse doit être
+        envoyée.
+        <br><br>Aucune information personnelle de l'utilisateur du site n'est publiée à l'insu de l'utilisateur,
+        échangée, transférée, cédée ou vendue sur un support quelconque à des tiers. Seule l'hypothèse du rachat du site
+        au propriétaire du site et de ses droits permettrait la transmission des dites informations à l'éventuel
+        acquéreur qui serait à son tour tenu de la même obligation de conservation et de modification des données vis à
+        vis de l'utilisateur du site www.lacabaneaburger.fr. Les bases de données sont protégées par les dispositions de
+        la loi du 1er juillet 1998 transposant la directive 96/9 du 11 mars 1996 relative à la protection juridique des
+        bases de données.
+      </p>
+    </div>
+  </div>
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: 'Mentions légales - La Cabane à Burger',
+})
+</script>
